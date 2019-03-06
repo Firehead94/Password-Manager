f9db1b6da65f13dd3e17061fa4cc9e9c922de0e8
Justin Scott <justinjonscott@gmail.com>
2019-03-06 16:31:10 -0500

    Update Main.java
    Added descriptions in prep for API construction on friday.

--- src/com/passwordmanager/Main.java
+++ src/com/passwordmanager/Main.java
@@ -9,11 +9,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
+            Expandable connection service objects. ConnectionPool is a
+            set of connection from this specific application. Each instance
+            of this application will have its own connectionpool that allows
+            multiple connections to be called from it. If web service created
+            based on this application, the same system can be used on server side
+            accross multiple connections from differing IPs.
+         */
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
+
+        /*
+            A prepared statement that is set using the Connection.prepareStatement
+            function. Function takes a string in the form of an SQL statement.
+         */
         PreparedStatement ps = null;
+
+        /*
+            Stores results from the DB. Is an object that contains information
+            of datatypes based on datatype in DB. call getType("ATTRIBUTE NAME")
+         */
         ResultSet rs = null;
 
+        /*
+            Test code to determine if DB connection has been esablished.
+         */
         String query = "SELECT * FROM TEST_TABLE";
 
         try {
