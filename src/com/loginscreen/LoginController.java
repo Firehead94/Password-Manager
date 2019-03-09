275857b09461073016ab54a49beb1045a11eb789
Patrick Kelly <pkelly@oakland.edu>
2019-03-09 16:30:46 -0500

    Login

---
+++ src/com/loginscreen/LoginController.java
@@ -0,0 +1,73 @@
+package com.loginscreen;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController
+{
+    @FXML
+    private AnchorPane login;
+
+    @FXML
+    private PasswordField passHiddenFld;
+
+    @FXML
+    private TextField passShowFld;
+
+    @FXML
+    private Button okBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button helpBtn;
+
+    @FXML
+    private CheckBox showCheckBox;
+
+    //Button Controllers. Temporary for now
+
+    @FXML
+    private void showHelp(ActionEvent actionEvent)
+    {
+        DialogBox.showInformation("Welcome To Our Password Manager",
+                                  "Eventually when you click here, we will give assistance on how " +
+                                           "to login. For now enjoy this cool custom dialog box.");
+
+    }
+
+    @FXML
+    private void showOK(ActionEvent actionEvent)
+    {
+        DialogBox.showInformation("Confirm",
+                                  "Clicking this button will allow you to login once we build our " +
+                                           "next scene.");
+    }
+
+    @FXML
+    private void closeApp(ActionEvent actionEvent)
+    {
+        System.exit(0);
+    }
+
+    public void toggleVisiblePassword(ActionEvent actionEvent)
+    {
+        //if show password checkbox is selected, show password in cleartext
+        if (showCheckBox.isSelected())
+        {
+            passShowFld.setText(passHiddenFld.getText());
+            passShowFld.setVisible(true);
+            passHiddenFld.setVisible(false);
+            return;
+        }
+
+        //otherwise don't show the password in cleartext
+        passHiddenFld.setText(passShowFld.getText());
+        passShowFld.setVisible(false);
+        passHiddenFld.setVisible(true);
+
+    }
+}
