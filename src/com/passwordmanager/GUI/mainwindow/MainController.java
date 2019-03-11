c354b3c2487129faa1873c63550a48f34791b0ef
Patrick Kelly <pkelly@oakland.edu>
2019-03-11 17:30:37 -0400

    merged main methods

---
+++ src/com/passwordmanager/GUI/mainwindow/MainController.java
@@ -0,0 +1,14 @@
+package com.passwordmanager.GUI.mainwindow;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuBar;
+
+public class MainController
+{
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private MenuBar menuBar;
+}
