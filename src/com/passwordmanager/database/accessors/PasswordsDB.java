264b876d7be3f1d8bf3950de63f813f61b9f8a1a
Justin Scott <justinjonscott@gmail.com>
2019-03-12 12:24:10 -0400

    DB Accessors User, Password, Folder

--- src/com/passwordmanager/database/accessors/PasswordsDB.java
+++ src/com/passwordmanager/database/accessors/PasswordsDB.java
@@ -1,27 +1,200 @@
 package com.passwordmanager.database.accessors;
 
 import com.passwordmanager.database.objects.Password;
+import com.passwordmanager.utils.DB;
+import com.sun.xml.internal.ws.util.StringUtils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PasswordsDB {
 
     // GET
-    public Password getPasswordByID(int id) {return null;}
-    public Password getPasswordByFolder(int id) {return null;}
-    public Password getPasswordByTitle(String title){return null;}
+    public Password getPassword(String attribute, int value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Password pwd = null;
 
-    public ArrayList<Password> getPasswords() {return null;}
+        String query = "SELECT * FROM PASSWORDS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, Integer.toString(value));
+            pwd = getFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return pwd;
+    }
+
+    public Password getPassword(String attribute, String value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Password pwd = null;
+
+        String query = "SELECT * FROM PASSWORDS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, value);
+            pwd = getFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return pwd;
+    }
+
+    public ArrayList<Password> getPasswords() {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<Password> pwds = null;
+
+        String query = "SELECT * FROM PASSWORDS";
+        try {
+            ps = connection.prepareStatement(query);
+            pwds = getListFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return pwds;
+    }
 
     // UPDATE
-    public boolean updatePassword(Password pwd) {return false;}
-    public boolean updatePassword(int id, String pwd) {return false;}
-    public boolean updateParentFolder(int id, int folderId) { return false;}
-    public boolean updatePasswordTitle(int id, String title) {return false;}
+    public boolean updatePassword(Password pwd) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        int retVal = 0;
+
+        String query = "UPDATE PASSWORDS SET " + DB.PASSWORD +
+                " = ?, " + DB.PASSWORD_TITLE +
+                " = ?, " + DB.PASSWORD_TIMESTAMP +
+                " = ? WHERE " + DB.PASSWORD_ID + " = ?;";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, pwd.getPassword());
+            ps.setString(2, pwd.getPassword_title());
+            ps.setLong(3, pwd.password_timestamp());
+            ps.setInt(4, pwd.getPassword_ID());
+            retVal = ps.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return retVal != 0;
+    }
 
 
     // INSERT
-    public boolean insertPassword(Password password) {return false;}
+    public boolean insertPassword(Password pwd) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        int retVal = 0;
+
+        String query = "INSERT INTO PASSWORDS (" +
+                DB.PASSWORD + ", " +
+                DB.PASSWORD_TITLE + ", " +
+                DB.PASSWORD_TIMESTAMP + ", " +
+                "VALUES (?,?,?);";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, pwd.getPassword());
+            ps.setString(2, StringUtils.capitalize(pwd.getPassword_title()));
+            ps.setLong(3, pwd.password_timestamp());
+            retVal = ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return retVal != 0;
+
+    }
+
+    // GENERIC
+    /**
+     * Gets a single password bean from the a DB based on a prepared statement
+     * @param ps
+     * @return
+     */
+    private static Password getFromDB(PreparedStatement ps) {
+
+        ResultSet rs = null;
+        Password pwd = null;
+        try {
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                pwd = new Password();
+
+                pwd.setPassword(rs.getString(DB.PASSWORD));
+                pwd.setFolder_ID(rs.getInt(DB.FOLDER_ID));
+                pwd.setPassword_title(rs.getString(DB.PASSWORD_TITLE));
+                pwd.setPassword_timestamp(rs.getInt(DB.PASSWORD_TIMESTAMP));
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+        }
+        return pwd;
+    }
+
+    /**
+     * Gets a list of password beans from a DB based on a prepared
+     * statement.
+     *
+     * @param ps
+     * @return
+     */
+    private static ArrayList<Password> getListFromDB(PreparedStatement ps) {
+        ResultSet rs = null;
+        ArrayList<Password> pwds = null;
+        try {
+            rs = ps.executeQuery();
+            pwds = new ArrayList<Password>();
+            while (rs.next()) {
+                Password pwd = new Password();
+
+                pwd.setPassword(rs.getString(DB.PASSWORD));
+                pwd.setFolder_ID(rs.getInt(DB.FOLDER_ID));
+                pwd.setPassword_title(rs.getString(DB.PASSWORD_TITLE));
+                pwd.setPassword_timestamp(rs.getInt(DB.PASSWORD_TIMESTAMP));
+
+                pwds.add(pwd);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(PasswordsDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+        }
+        return pwds;
+    }
 
 
 }
--- src/com/passwordmanager/database/accessors/UserDB.java
+++ src/com/passwordmanager/database/accessors/UserDB.java
@@ -1,30 +1,306 @@
 package com.passwordmanager.database.accessors;
 import java.util.ArrayList;
-
+import java.sql.*;
 import com.passwordmanager.database.objects.*;
+import com.passwordmanager.utils.DB;
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDB {
 
-    // GET
-    public User getUserByID(int id) {return null;}
-    public User getUserByUsername(String username) {return null;}
+    /**
+     * Validates a users login information. Will return true if the user has
+     * entered the correct credentials.
+     *
+     * @param username Username entered in by user
+     * @param hash UD5 hash generated from password
+     * @return
+     */
+    public static boolean validateUser(String username, String hash) {
 
-    public ArrayList<User> getUsers() {return null;}
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Boolean retval = false;
+
+        String query = "SELECT COUNT(*) FROM USERS " +
+                "WHERE USER_USERNAME = ? AND USER_PASSWORD = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, hash);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                retval = true;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return retval;
+
+    }
+    public static boolean userExists(String username) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Boolean flag = false;
+
+        String query = "SELECT * FROM USERS " +
+                "WHERE " + DB.USER_USERNAME + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                flag = true;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+            return flag;
+        }
+    }
+
+    // GET
+    public User getUser(String attribute, int value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        User user = null;
+
+        String query = "SELECT * FROM USERS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, Integer.toString(value));
+            user = getFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return user;
+    }
+    public User getUser(String attribute, String value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        User user = null;
+
+        String query = "SELECT * FROM USERS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, value);
+            user = getFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return user;
+    }
+    public ArrayList<User> getUsers() {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<User> users = null;
+
+        String query = "SELECT * FROM USERS";
+        try {
+            ps = connection.prepareStatement(query);
+            users = getListFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return users;
+    }
+    public ArrayList<User> getUsers(String attribute, int value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<User> users = null;
+
+        String query = "SELECT * FROM USERS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, Integer.toString(value));
+            users = getListFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return users;
+    }
+    public ArrayList<User> getUsers(String attribute, String value) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<User> users = null;
+
+        String query = "SELECT * FROM USERS " +
+                "WHERE " + attribute + " = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, value);
+            users = getListFromDB(ps);
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return users;
+    }
 
     // UPDATE
-    public boolean updateUser(User user) {return false;}
-    public boolean updateUsername(int id, String username){return false;}
-    public boolean updateUsername(String  oldUsername, String newUsername){return false;}
-    public boolean updatePassword(int id, String pwd){return false;}
-    public boolean updatePassword(String  username, String pwd){return false;}
-    public boolean updateFirstname(int id, String fname){return false;}
-    public boolean updateFirstname(String  username, String fname){return false;}
-    public boolean updateLastname(int id, String lname){return false;}
-    public boolean updateLastname(String  username, String lname){return false;}
-    public boolean updateAccessLevel(int id, int access){return false;}
-    public boolean updateAccessLevel(String  username, int access){return false;}
+    public boolean updateUser(User user) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        int retVal = 0;
+
+        String query = "UPDATE USERS SET " + DB.USER_USERNAME +
+                " = ?, " + DB.USER_FIRSTNAME +
+                " = ?, " + DB.USER_LASTNAME +
+                " = ?, " + DB.ACCESS_LEVEL +
+                " = ?, " + DB.USER_PASSWORD +
+                " = ? WHERE " + DB.USER_ID + " = ?;";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, user.getUser_username());
+            ps.setString(2, user.getUser_first_name());
+            ps.setString(3, user.getUser_last_name());
+            ps.setInt(4, user.getAccess_level());
+            ps.setString(5, user.getUser_password());
+            ps.setInt(6, user.getUser_ID());
+            retVal = ps.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return retVal != 0;
+    }
 
     // INSERT
-    public boolean insertUser(User user) {return false;}
+    public boolean insertUser(User user) {
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        int retVal = 0;
+
+        String query = "INSERT INTO USERS (" +
+        DB.USER_USERNAME + ", " +
+        DB.USER_FIRSTNAME + ", " +
+        DB.USER_LASTNAME + ", " +
+        DB.USER_PASSWORD + ") " +
+                "VALUES (?,?,?,?);";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, StringUtils.capitalize(user.getUser_username()));
+            ps.setString(2, StringUtils.capitalize(user.getUser_first_name()));
+            ps.setString(3, StringUtils.capitalize(user.getUser_last_name()));
+            ps.setString(4, user.getUser_password());
+            retVal = ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            DBUtils.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        return retVal != 0;
+
+    }
+
+
+    // GENERIC
+    /**
+     * Gets a single user bean from the a DB based on a prepared statement
+     * @param ps
+     * @return
+     */
+    private static User getFromDB(PreparedStatement ps) {
+
+        ResultSet rs = null;
+        User user = null;
+        try {
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                user = new User();
+
+                user.setUser_ID(rs.getInt(DB.USER_ID));
+                user.setUser_username(rs.getString(DB.USER_USERNAME));
+                user.setUser_first_name(rs.getString(DB.USER_FIRSTNAME));
+                user.setUser_last_name(rs.getString(DB.USER_LASTNAME));
+                user.setAccess_level(rs.getInt(DB.ACCESS_LEVEL));
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+        }
+        return user;
+    }
+
+    /**
+     * Gets a list of user beans from a DB based on a prepared
+     * statement.
+     *
+     * @param ps
+     * @return
+     */
+    private static ArrayList<User> getListFromDB(PreparedStatement ps) {
+        ResultSet rs = null;
+        ArrayList<User> users = null;
+        try {
+            rs = ps.executeQuery();
+            users = new ArrayList<User>();
+            while (rs.next()) {
+                User user = new User();
+
+                user.setUser_ID(rs.getInt(DB.USER_ID));
+                user.setUser_username(rs.getString(DB.USER_USERNAME));
+                user.setUser_first_name(rs.getString(DB.USER_FIRSTNAME));
+                user.setUser_last_name(rs.getString(DB.USER_LASTNAME));
+                user.setAccess_level(rs.getInt(DB.ACCESS_LEVEL));
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtils.closeResultSet(rs);
+        }
+        return users;
+    }
 
 }
