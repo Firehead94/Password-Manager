c2d19b330ee5db6bff186c4bc5a5cb35ef057349
Firehead94 <justinjonscott@gmail.com>
2019-03-18 00:51:02 -0400

    comment

--- src/com/passwordmanager/gui/controllers/FolderBuilder.java
+++ src/com/passwordmanager/gui/controllers/FolderBuilder.java
@@ -15,6 +15,12 @@ public class FolderBuilder {
     private static Map<Integer, TreeItem<Folder>> mapToParents = new HashMap<>();
     private static Map<Integer, TreeItem<Folder>> mapToID = new HashMap<>();
 
+    /**
+     * Builds a treeItem out of an arraylist populated by database.
+     * 
+     * @param folders
+     * @return
+     */
     public static TreeItem<Folder> buildTreeView(ArrayList<Folder> folders) {
 
         TreeItem<Folder> root = new TreeItem<>(new Folder());
@@ -27,6 +33,15 @@ public class FolderBuilder {
         return addLeaves(root, folders);
     }
 
+    /**
+     * Recursively builds folder structure by navigating through possible children.
+     * Is not memory efficient what so ever but should be fine for the majority of
+     * users.
+     *
+     * @param root
+     * @param folders list of all folders
+     * @return returns on leaf of tree
+     */
     public static TreeItem<Folder> addLeaves(TreeItem<Folder> root, ArrayList<Folder> folders) {
         for (Folder folder : folders)
             if (folder.getFolder_parent() == root.getValue().getFolder_ID())
